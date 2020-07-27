04cf0b50009983fee54740c451af7ea29d487e35
Peiran Li <pelny@google.com>
2020-07-27 07:10:40 +0000

    Add docstrings

--- data/utils.py
+++ data/utils.py
@@ -23,7 +23,18 @@ from requests.packages.urllib3.util.retry import Retry
 def send_request(url: str,
                  params: dict = None,
                  headers: dict = None,
-                 auth: Tuple[str, str] = None) -> Union[List[dict], dict]:
+                 auth: Tuple[str, str] = None) -> Union[List[dict], dict, None]:
+    """Performs HTTP GET request with url, parameters, and headers.
+
+    Args:
+        url: A str of the HTTP endpoint.
+        params: A dict of HTTP request parameters.
+        headers: A dict of HTTP request headers.
+        auth: A tuple of username, token.
+    Returns:
+        The json_response can be either a dict or a list of dicts, depending on
+        the actual returned response. Or None, if an error occurred.
+    """
     try:
         session = requests.Session()
         retry = Retry(connect=5, backoff_factor=0.5)
@@ -38,21 +49,24 @@ def send_request(url: str,
         return json_response
     except requests.exceptions.HTTPError as http_error:
         print("Http Error:", http_error)
-        return []
     except requests.exceptions.ConnectionError as connection_error:
         print("Error Connecting:", connection_error)
-        return []
     except requests.exceptions.Timeout as timeout_error:
         print("Timeout Error:", timeout_error)
-        return []
     except requests.exceptions.RequestException as request_exception:
         print("Request Exception:", request_exception)
-        return []
 
 
-# Get a list of repository names by username
 def get_all_repositories(
         username: str, auth: Tuple[str, str] = None) -> List[str]:
+    """Retrieves a complete list of repository names by username.
+
+    Args:
+        username: A str of owner login.
+        auth: A tuple of username, token.
+    Returns:
+        A complete list of repository names.
+    """
     repo_names = []
     for page in count(1):
         repo_names_by_page = get_repositories_by_page(page, username, auth)
@@ -64,6 +78,15 @@ def get_all_repositories(
 
 def get_repositories_by_page(
         page: int, username: str, auth: Tuple[str, str] = None) -> List[str]:
+    """Retrieves a list of repository names on certain page.
+
+    Args:
+        page: An integer indicating which page to retrieve.
+        username: A str of owner login.
+        auth: A tuple of username, token.
+    Returns:
+        A list of repository names.
+    """
     url = "https://api.github.com/users/%s/repos" % username
     query_parameters = {'page': page}
     json_response = send_request(url=url, params=query_parameters, auth=auth)
@@ -76,17 +99,33 @@ def get_repositories_by_page(
     return repo_names_by_page
 
 
-def save_repositories(username: str, repo_names: str) -> None:
+def save_repositories(username: str, repo_names: List[str]) -> None:
+    """Saves the repository names to text file.
+
+    Args:
+        username: A str of owner login.
+        repo_names: A list of repository names.
+    Returns:
+    """
     with open('./%s_repos.txt' % username, 'w') as file:
         for repo in repo_names:
             file.write(repo)
             file.write('\n')
 
 
-# Get a list of pull requests by repo_name
 def get_all_pull_requests(repo_name: str,
                           state: str = 'closed',
                           auth: Tuple[str, str] = None) -> List[dict]:
+    """Retrieves a complete list of pull requests information by repository
+    name.
+
+    Args:
+        repo_name: A str of repository name.
+        state: A str of pull request state. Values can be 'open' or 'closed'.
+        auth: A tuple of username, token.
+    Returns:
+         A list of dicts. Each dict holds a pull request information.
+    """
     pull_requests = []
     for page in count(1):
         pull_requests_by_page = get_pull_requests_by_page(
@@ -101,6 +140,16 @@ def get_pull_requests_by_page(page: int,
                               repo_name: str,
                               state: str = 'closed',
                               auth: Tuple[str, str] = None) -> List[dict]:
+    """Retrieves a list of pull requests information on a certain page.
+
+    Args:
+        page: An integer indicating which page to retrieve.
+        repo_name: A str of repository name.
+        state: A str of pull request state. Values can be 'open' or 'closed'.
+        auth: A tuple of username, token.
+    Returns:
+        A list of dicts. Each dict holds a pull request information.
+    """
     url = "https://api.github.com/repos/%s/pulls" % repo_name
     query_parameters = {'page': page, 'state': state}
     json_response = send_request(url=url, params=query_parameters, auth=auth)
@@ -110,6 +159,15 @@ def get_pull_requests_by_page(page: int,
 
 
 def save_pull_requests(repo_name: str, pull_requests: List[dict]) -> None:
+    """Saves a list of pull requests information to text file.
+
+    Args:
+        repo_name: A str of repository name.
+        pull_requests: A list of dicts. Each dict holds a pull request
+            information.
+    Returns:
+        None.
+    """
     with open('./%s_pull_requests.txt' % repo_name, 'w') as file:
         for pull_request in pull_requests:
             file.write(str(pull_request))
@@ -119,27 +177,60 @@ def save_pull_requests(repo_name: str, pull_requests: List[dict]) -> None:
 def get_pull_request_info(repo_name: str,
                           pull_request_number: int,
                           auth: Tuple[str, str] = None) -> dict:
+    """Retrieves pull request information.
+
+    Retrieves pull request information of given repository name, pull request
+    id. Authentication is optional.
+
+    Args:
+        repo_name: A str of repository name.
+        pull_request_number: An integer of pull request id.
+        auth: A tuple of username, token.
+    Returns:
+         A dict of pull request information.
+    """
     url = "https://api.github.com/repos/%s/pulls/%s" % (
         repo_name, pull_request_number)
     return send_request(url=url, auth=auth)
 
 
-# Pull request review comments are comments on a portion of the unified diff
-# made during a pull request review.
 def get_pull_request_review_comments(repo_name: str,
                                      pull_request_number: int,
                                      auth: Tuple[str, str] = None
                                      ) -> List[dict]:
+    """Retrieves a list of pull request review comments.
+
+    Pull request review comments are comments on a portion of the unified diff
+    made during a pull request review.
+
+    Args:
+        repo_name: A str of repository name.
+        pull_request_number: An integer of pull request id.
+        auth: A tuple of username, token.
+    Returns:
+        A list of dicts. Each dict holds a pull request review comment
+            information.
+    """
     url = "https://api.github.com/repos/%s/pulls/%s/comments" % (
         repo_name, pull_request_number)
     return send_request(url=url, auth=auth)
 
 
-# Pull Request Reviews are groups of Pull Request Review Comments on the Pull
-# Request, grouped together with a state and optional body comment.
 def get_pull_request_reviews(repo_name: str,
                              pull_request_number: int,
                              auth: Tuple[str, str] = None) -> List[dict]:
+    """Retrieves a list of pull request review information.
+
+    Pull Request Reviews are groups of Pull Request Review Comments on the Pull
+    Request, grouped together with a state and optional body comment.
+
+    Args:
+        repo_name: A str of repository name.
+        pull_request_number: An integer of pull request id.
+        auth: A tuple of username, token.
+    Returns:
+        A list of dicts. Each dict holds a pull request review information.
+    """
     url = "https://api.github.com/repos/%s/pulls/%s/reviews" % (
         repo_name, pull_request_number)
     return send_request(url=url, auth=auth)
@@ -148,6 +239,15 @@ def get_pull_request_reviews(repo_name: str,
 def get_pull_request_commits(repo_name: str,
                              pull_request_number: int,
                              auth: Tuple[str, str] = None) -> List[dict]:
+    """Retrieves a list of pull request commits information.
+
+    Args:
+        repo_name: A str of repository name.
+        pull_request_number: An integer of pull request id.
+        auth: A tuple of username, token.
+    Returns:
+        A list of dicts. Each dict holds a pull request commit information.
+    """
     url = "https://api.github.com/repos/%s/pulls/%s/commits" % (
         repo_name, pull_request_number)
     return send_request(url=url, auth=auth)
@@ -164,6 +264,16 @@ def get_pull_request_files(repo_name: str,
 def get_pull_request_issue_comments(repo_name: str,
                                     pull_request_number: int,
                                     auth: Tuple[str, str] = None) -> List[dict]:
+    """Retrieves a list of pull request issue comments information.
+
+    Args:
+        repo_name: A str of repository name.
+        pull_request_number: An integer of pull request id.
+        auth: A tuple of username, token.
+    Returns:
+        A list of dicts. Each dict holds a pull request issue comment
+            information.
+    """
     url = "https://api.github.com/repos/%s/issues/%s/comments" % (
         repo_name, pull_request_number)
     return send_request(url=url, auth=auth)
@@ -172,6 +282,15 @@ def get_pull_request_issue_comments(repo_name: str,
 def get_commit_info(repo_name: str,
                     commit_ref: str,
                     auth: Tuple[str, str] = None) -> dict:
+    """Retrieves a commit information.
+
+    Args:
+        repo_name: A str of repository name.
+        commit_ref: A str of commit id.
+        auth: A tuple of username, token.
+    Returns:
+        A dict of commit information.
+    """
     url = "https://api.github.com/repos/%s/commits/%s" % (repo_name, commit_ref)
     return send_request(url=url, auth=auth)
 
@@ -179,6 +298,15 @@ def get_commit_info(repo_name: str,
 def get_commit_check_runs(repo_name: str,
                           commit_ref: str,
                           auth: Tuple[str, str] = None) -> dict:
+    """Retrieves check run results for a commit.
+
+    Args:
+        repo_name: A str of repository name.
+        commit_ref: A str of commit id.
+        auth: A tuple of username, token.
+    Returns:
+        A dict of check run results.
+    """
     url = "https://api.github.com/repos/%s/commits/%s/check-runs" % (
         repo_name, commit_ref)
     headers = {'Accept': 'application/vnd.github.antiope-preview+json'}
@@ -188,6 +316,15 @@ def get_commit_check_runs(repo_name: str,
 def is_pull_request_merged(repo_name: str,
                            pull_request_number: int,
                            auth: Tuple[str, str] = None) -> bool:
+    """Checks whether the pull request is merged.
+
+    Args:
+        repo_name: A str of repository name.
+        pull_request_number: An integer of pull request id.
+        auth: A tuple of username, token.
+    Returns:
+         A boolean indicating whether the pull request is merged.
+    """
     url = "https://api.github.com/repos/%s/pulls/%s/merge" % (
         repo_name, pull_request_number)
     response = requests.get(url=url, auth=auth)
@@ -196,11 +333,26 @@ def is_pull_request_merged(repo_name: str,
 
 def get_user_public_events(username: str,
                            auth: Tuple[str, str] = None) -> List[dict]:
+    """Retrieves the public events of a username login.
+
+    Args:
+        username: A str of username login.
+        auth: A tuple of username, token.
+    Returns:
+        A list of dicts. Each dict holds the past events for a user.
+    """
     url = "https://api.github.com/users/%s/events" % username
     return send_request(url=url, auth=auth)
 
 
 def to_timestamp(time_str: str) -> float:
+    """Converts ISO time str to timestamp.
+
+    Args:
+        time_str: A str of ISO time.
+    Returns:
+         A float of timestamp.
+    """
     date, time = time_str[:-1].split('T')
     year, month, day = map(int, date.split('-'))
     hour, minute, second = map(int, time.split(':'))

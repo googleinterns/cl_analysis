5e5238fc6e469b75079679900331c734e3ac6d5b
Peiran Li <pelny@google.com>
2020-08-29 03:35:44 +0000

    Handle the case where the file does not exist on that date

--- data/feature_extraction.py
+++ data/feature_extraction.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import argparse
+import os
 import pandas as pd
 from datetime import datetime, timedelta
 
@@ -337,8 +338,10 @@ def main(arguments):
         .to_pydatetime().tolist()
     for date in dates:
         date_str = date.strftime(format="%Y_%m_%d")
-        print("Extracting features on %s" % date_str)
         file_name = './%s_%s.csv' % (arguments.repo, date_str)
+        if not os.path.exists(file_name):
+            continue
+        print("Extracting features on %s" % date_str)
         file_level_data = pd.read_csv(file_name)
         feature_extractor = FeatureExtractor(file_level_data, date_str)
         feature_extractor.extract_features()
